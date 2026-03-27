a21cabf8aa3c630bd4e84c78d1359b547754b9a3
rahul.cyber <rahul.cyber2023@outlook.com>
2026-03-27 19:02:05 +0530

    leave managmenet created

--- resources.php
+++ resources.php
@@ -98,9 +98,9 @@ $qs  = '&search=' . urlencode($search);
                             </span>
                         </td>
                         <td class="text-end">
-                            <!-- <a href="leave_management.php?id=<?= $r['id'] ?>" class="pms-action-btn me-1" title="Leave Management" style="color: #0284c7; border-color: #7dd3fc; background: #e0f2fe;">
+                            <a href="leave_management.php?id=<?= $r['id'] ?>" class="pms-action-btn me-1" title="Leave Management" style="color: #0284c7; border-color: #7dd3fc; background: #e0f2fe;">
                                 <i class="bi bi-calendar-event-fill"></i>
-                            </a> -->
+                            </a>
                             
                             <a href="view_resource.php?id=<?= $r['id'] ?>" class="pms-action-btn me-1" title="Edit">
                                 <i class="bi bi-eye"></i>
